8950781b87b9dd6f684d78e99abcfb07d459aa88
Eugee29 <uri2920@gmail.com>
2024-10-20 13:31:28 +0300

    Build errors fix

--- app/actions.ts
+++ app/actions.ts
@@ -2,7 +2,7 @@
 
 import { COMFY_SERVER_URL } from '@/lib/constants'
 import { ComfyResponse } from '@/lib/definitions'
-import workflow from '@/workflow-sd1.5.json'
+import workflow from '@/workflows/workflow-sd1.5.json'
 import axios from 'axios'
 
 export async function queuePrompt(prompt: string, clientId: string) {
